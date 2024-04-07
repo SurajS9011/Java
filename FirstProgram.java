a9d89b1c02e652031affdb6ed515f72346a68952
SurajS9011 <166317495+SurajS9011@users.noreply.github.com>
2024-04-07 21:24:45 +0530

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+Package com.company;
+public class Main {
+    public static  void main(String[] args) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        String str= "harry";
+        System.out.println(str);
+        int age= 24;
+        
+
+
+
+
+    }
+}
+
